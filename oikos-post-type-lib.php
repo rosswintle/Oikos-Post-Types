49fd62942ff17b91c7d13d0c0cf6cc2729bfc0a9
Ross Wintle <ross@oikos.org.uk>
2016-12-15 12:27:48 +0000

    Fixed PHP warnings. The template method signatures weren't compatible with the parent signatures. Inheriting is not needed anyway.

--- oikos-post-type-lib.php
+++ oikos-post-type-lib.php
@@ -189,28 +189,27 @@ class Oikos_Post_Type {
 }
 
 
-class Oikos_Post_Type_Template extends Oikos_Post_Type {
+class Oikos_Post_Type_Template {
 	
-	// Be sure to refer to this using static:: as it needs to be the value of the child class
 	private static $post_type;
 		
 	public static function get( $id ) {
-		return parent::get( $id );
+		return Oikos_Post_Type::get( $id );
 	}
 	public static function get_all( $options=array() ) {
-		return parent::get_all( static::$post_type, $options );
+		return Oikos_Post_Type::get_all( self::$post_type, $options );
 	}
 	public static function get_recent( $options=array() ) {
-		return parent::get_recent( static::$post_type, $options );
+		return Oikos_Post_Type::get_recent( self::$post_type, $options );
 	}
 	public static function get_list( $options=array(), $no_value_label='' ) {
-		return parent::get_list( static::$post_type, $options, $no_value_label );
+		return Oikos_Post_Type::get_list( self::$post_type, $options, $no_value_label );
 	}
 	public static function get_posts_by_meta( $meta_key='', $meta_value='', $options=array() ) {
-		return parent::get_posts_by_meta( static::$post_type, $meta_key, $meta_value, $options);
+		return Oikos_Post_Type::get_posts_by_meta( self::$post_type, $meta_key, $meta_value, $options);
 	}
 	public static function get_posts_by_meta_query( $meta_query, $options=array() ) {
-		return parent::get_posts_by_meta_query( static::$post_type, $meta_query, $options);
+		return Oikos_Post_Type::get_posts_by_meta_query( self::$post_type, $meta_query, $options);
 	}
 	
 }
